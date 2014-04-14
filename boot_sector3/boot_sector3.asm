5242b64530af3f4bd6b9f10c2876fd4b851e48ea
William Woodruff <william@tuffbizz.com>
2014-04-13 21:38:30 -0400

    added boot_sector3

---
+++ boot_sector3/boot_sector3.asm
@@ -0,0 +1,38 @@
+;	boot_sector3.asm
+;	A 512-byte boot sector that prints "Hello, World!" using a defined function
+;	Author: William Woodruff
+
+print:
+	mov ah, 0x0e
+	int 0x10
+	ret
+
+mov al, 'H'
+call print
+mov al, 'e'
+call print
+mov al, 'l'
+call print
+mov al, 'l'
+call print
+mov al, 'o'
+call print
+mov al, ','
+call print
+mov al, 'W'
+call print
+mov al, 'o'
+call print
+mov al, 'r'
+call print
+mov al, 'l'
+call print
+mov al, 'd'
+call print
+mov al, '!'
+call print
+
+jmp $
+
+times 510-($-$$) db 0
+dw 0xaa55
