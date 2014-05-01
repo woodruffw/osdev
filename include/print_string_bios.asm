1bdd6ec1767053b256a07645ce24d84109bd2787
William Woodruff <william@tuffbizz.com>
2014-05-01 10:27:06 -0400

    fixed missing line

--- include/print_string_bios.asm
+++ include/print_string_bios.asm
@@ -1,12 +1,11 @@
 ;	print_string_bios
 ;	prints a null-terminated string pointed to by the si register
 ;	IMPORTANT: string MUST be null-terminated
-[bits 32]
 print_string_bios:
 	mov ah, 0x0e
 	.next:
 		mov al, [si]
-		
+		cmp al, 0
 		je .done
 		int 0x10
 		jmp .next
